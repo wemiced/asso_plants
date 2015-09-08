cf357c665cd5aea1aa356cd8c4ab7a020a98d029
poupi <tfrere>
2015-09-08 05:02:11 +0200

    big up gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,12 @@
   var livereload    = require('gulp-livereload');
   var svgmin        = require('gulp-svgmin');
   var runSequence   = require('gulp-run-sequence');
+  var concat        = require('gulp-concat');
+  var jsmin         = require('gulp-jsmin');
+  var del           = require('del');
+  var rename        = require('gulp-rename');
+  var connect       = require('gulp-connect');
+
 
 /* ## Paths
 ================================================== */
@@ -15,10 +21,46 @@
       styles: {
           src: './src',
           files:  './src/styles/*.scss',
-          dest: './public/'
+          dest: './public/styles/',
+          watch: './src/styles/**/*.scss'
+      },
+      jsminifier: {
+          files: './src/vendor/*.js',
+          dest: './src/vendor/min/'
+      },
+      jsconcat : {
+        files: './src/vendor/min/*.js',
+        filename: 'vendor.min.js',
+        dest: './public/vendor/'
+      },
+      svgmin : {
+        file: './src/assets/svg/*.svg',
+        dest: './public/assets/svg/'
       }
-
   }
+
+/* ## clean
+================================================== */
+
+  gulp.task('clean', function () {
+    return del(['public/**'] );
+  });
+
+
+/* ## serve
+================================================== */
+
+gulp.task('serve', function() {
+
+    connect.server({
+        port:8000,
+        root: './public',
+        livereload: {
+            port: 21730
+        }
+    });
+
+});
 
 /* ## displayErrors instead of crash
 ================================================== */
@@ -30,11 +72,10 @@
 
       if(error.fileName)
           errorString += ' in ' + error.fileName;
-
       if(error.lineNumber)
           errorString += ' on line ' + error.lineNumber;
-      console.error(errorString);
 
+      console.error(errorString);
   }
 
 /* ## Sass
@@ -42,7 +83,7 @@
 
   gulp.task('sass', function (){
       return gulp.src(paths.styles.files)
-      .pipe(sass())
+      .pipe(sass({minify:true}))
       .on('error', function(err){
           displayError(err);
       })
@@ -54,7 +95,7 @@
 ================================================== */
 
   gulp.task('svgmin', function() {
-    return gulp.src('./src/assets/*.svg')
+    return gulp.src(paths.svgmin.file)
         .pipe(svgmin({
             plugins: [{
                 removeDoctype: false
@@ -62,14 +103,30 @@
                 removeComments: false
             }]
         }))
-        .pipe(gulp.dest('./public/assets/'));
+        .pipe(gulp.dest(paths.svgmin.dest))
+        .pipe(livereload());
   });
 
 
-  gulp.task('livereload', function() {
-    return  gulp.src('').pipe(livereload());
+/* ## concat
+================================================== */
+
+  gulp.task('concat', function() {
+    return gulp.src(paths.jsconcat.files)
+      .pipe(concat(paths.jsconcat.filename))
+      .pipe(gulp.dest(paths.jsconcat.dest));
   });
 
+
+/* ## js minifier
+================================================== */
+
+  gulp.task('js-minify', function () {
+      return gulp.src(paths.jsminifier.files)
+          .pipe(jsmin())
+          .pipe(rename({suffix: '.min'}))
+          .pipe(gulp.dest(paths.jsminifier.dest));
+  });
 
 /* ## Watch
 ================================================== */
@@ -77,24 +134,54 @@
   gulp.task('watch', function() {
     livereload({ start: true });
     livereload.listen();
-    gulp.watch(['./src/styles/**/*.scss'], ['sass']);
+    gulp.watch([paths.styles.watch], ['sass']);
+    gulp.watch(['./src/assets/fonts/*', './src/index.html', './src/datas/association.js', './src/assets/png/*', './src/scripts/app.js'], ['files']);
+    gulp.watch(['./src/assets/svg/*.svg'], ['svgmin']);
+    gulp.watch(['./src/vendor/*.js'], ['build-js']);
+
+  });
+
+/* ## files
+================================================== */
+
+  gulp.task('files', function(cb){
+      gulp.src('./src/index.html')
+      .pipe(gulp.dest('./public/'));
+
+      gulp.src('./src/assets/font/*')
+      .pipe(gulp.dest('./public/assets/font'));
+
+      gulp.src('./src/datas/association.js')
+      .pipe(gulp.dest('./public/datas/'));
+
+      gulp.src('./src/scripts/app.js')
+      .pipe(gulp.dest('./public/scripts/'));
+
+      gulp.src('./src/assets/png/*.png')
+      .pipe(gulp.dest('./public/assets/png'))
+      .pipe(livereload());
+      cb();
   });
 
 /* ## Run sequences
 ================================================== */
 
+  gulp.task('build-js', function(cb) {
+    runSequence('js-minify', 'concat', cb);
+  });
+
   gulp.task('build-dev', function(cb) {
-    runSequence('sass', cb);
+    runSequence(['clean', 'sass', 'js-minify', 'files'], 'concat', cb);
   });
 
   gulp.task('build-prod', function(cb) {
-    runSequence(['sass', 'svgmin'], cb);
+    runSequence(['js-minify', 'svgmin', 'files'], 'sass', 'concat', cb);
   });
 
 /* ## Options
 ================================================== */
-
-  gulp.task('default', ['watch']);
+  gulp.task('default', ['watch', 'files', 'serve']);
+  gulp.task('clean', ['clean']);
   gulp.task('dev'), ['build-dev'];
   gulp.task('production', ['build-prod']);
 
---
+++ src/datas/association.js
@@ -0,0 +1,299 @@
+window.plants = [
+  {
+    "name": "Ail",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/ail.svg",
+    "desc": "blabla bla blabla blalblalbla sqd qd qsdq dqs dqs",
+    "good": ["Asperge", "Betterave", "Concombre", "Fraisier", "Laitue", "Tomate"],
+    "bad": ["Haricot", "Pois"]
+  },
+  {
+    "name": "Aneth",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/aneth.svg",
+    "desc": "blabla bla blabla blalblalbla eazeaz gfdfg ddfgdfg",
+    "good": [],
+    "bad": ["Carotte"]
+  },
+  {
+    "name": "Artichaut",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/artichaut.svg",
+    "desc": "blabla bla blabla blalblalbla sqd qdsaze yrtyhgf ",
+    "good": ["Haricot", "Laitue", "Oignon", "Pois", "Radis"],
+    "bad": []
+  },
+  {
+    "name": "Asperge",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/asperge.svg",
+    "desc": "blabla bla blabla blalblalbla iopikjhjhghjg g jg fh d fgd ",
+    "good": ["Ail", "Chou", "Haricot", "Laitue", "Navet", "Oignon", "Persil", "Pois", "Radis", "Tomate"],
+    "bad": []
+  },
+  {
+    "name": "Aubergine",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/aubergine.svg",
+    "desc": "blabla bla blabla blalblalbla azea zeaza eaz ea",
+    "good": ["Haricot"],
+    "bad": []
+  },
+  {
+    "name": "Bette",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/bette.svg",
+    "desc": "blabla bla blabla blalblalbla favav  vavava ",
+    "good": ["Carotte"],
+    "bad": []
+  },
+  {
+    "name": "Betterave",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/betterave.svg",
+    "desc": "blabla bla blabla blalblalbla wxcxx regt r hgtr",
+    "good": ["Ail", "Chou", "Concombre", "Haricot", "Navet", "Oignon", "Pois"],
+    "bad": ["Mais"]
+  },
+  {
+    "name": "Carotte",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/carotte.svg",
+    "desc": "blabla bla blabla blalblalbla dqs hfhytr iyuhjgfds",
+    "good": ["Bette", "Ciboulette", "Haricot", "Laitue", "Navet", "Oignon", "Poireau", "Pois", "Radis", "Tomate"],
+    "bad": ["Aneth", "Pomme de terre"]
+  },
+  {
+    "name": "Céleri",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/celeri.svg",
+    "desc": "blabla bla blabla blalblalbla bcnvxxc qsqd aqdzefdss dqq",
+    "good": ["Chou", "Poireau", "Radis", "Tomate"],
+    "bad": []
+  },
+  {
+    "name": "Cerfeuil",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/cerfeuil.svg",
+    "desc": "blabla bla blabla blalblalbla uiyj tyjytj ukfdsqs",
+    "good": ["Radis"],
+    "bad": []
+  },
+  {
+    "name": "Chou",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/chou.svg",
+    "desc": "blabla bla blabla blalblalbla loikjhg jhjghlkhj xwcxwvx",
+    "good": ["Asperge", "Betterave", "Cerfeuil", "Concombre", "Epinard", "Haricot", "Laitue", "Pois", "Pomme de terre", "Tomate"],
+    "bad": ["Fenouil", "Fraisier", "Radis"]
+  },
+  {
+    "name": "Ciboulette",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/ciboulette.svg",
+    "desc": "blabla bla blabla blalblalbla azedsqxcvx dfzefzdsqd adzad",
+    "good": ["Carotte"],
+    "bad": ["Haricot", "Pois"]
+  },
+  {
+    "name": "Concombre",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/concombre.svg",
+    "desc": "blabla bla blabla blalblalbla ilkiyujyrt ytree sqdsdqd",
+    "good": ["Ail", "Betterave", "Chou", "Haricot", "Mais", "Oignon", "Pois", "Radis"],
+    "bad": ["Pomme de terre", "Tomate"]
+  },
+  {
+    "name": "Courge",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/courge.svg",
+    "desc": "blabla bla blabla blalblalbla d ad azdza dsqdqfdsfs qqdsqds az",
+    "good": ["Haricot", "Laitue", "Mais", "Navet"],
+    "bad": ["Pomme de terre"]
+  },
+  {
+    "name": "Potiron",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/potiron.svg",
+    "desc": "blabla bla blabla blalblalbla moilukyj jyuujtrtfdsqqd ",
+    "good": ["Haricot", "Laitue", "Mais", "Navet"],
+    "bad": ["Pomme de terre"]
+  },
+  {
+    "name": "Courgette",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/courgette.svg",
+    "desc": "blabla bla blabla blalblalbla  azeds ds sdzeferyiytfdscsdf",
+    "good": ["Haricot", "Laitue", "Mais", "Navet"],
+    "bad": ["Pomme de terre"]
+  },
+  {
+    "name": "Epinard",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/epinard.svg",
+    "desc": "blabla bla blabla blalblalbla azedsfdsf fsd sdfefefz",
+    "good": ["Chou", "Fraisier", "Haricot", "Oignon", "Poireau", "Pois", "Radis"],
+    "bad": []
+  },
+  {
+    "name": "Fenouil",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/fenouil.svg",
+    "desc": "blabla bla blabla blalblalbla liujyth tjyrezds",
+    "good": [],
+    "bad": ["Chou", "Tomate"]
+  },
+  {
+    "name": "Feve",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/feve.svg",
+    "desc": "blabla bla blabla blalblalbla olikuyhilkujyt rerzfe",
+    "good": ["Pomme de terre"],
+    "bad": []
+  },
+  {
+    "name": "Fraisier",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/fraisier.svg",
+    "desc": "blabla bla blabla blalblalbla  fezfdsffds iuoulkyursd dsdq",
+    "good": ["Ail", "Oignon"],
+    "bad": ["Chou"]
+  },
+  {
+    "name": "Haricot",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/haricot.svg",
+    "desc": "blabla bla blabla blalblalbla  dzadsq fs gfdgdsd sqdqd qsd",
+    "good": ["Artichaut", "Asperge", "Aubergine", "Betterave", "Carotte", "Chou", "Concombre", "Courge", "Potiron", "Courgette", "Epinard", "Poireau", "Pomme de terre"],
+    "bad": ["Ail", "Ciboulette", "Oignon", "Persil", "Radis", "Tomate"]
+  },
+  {
+    "name": "Laitue",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/laitue.svg",
+    "desc": "blabla bla blabla blalblalbla  dazza dqs qsqdq zdqzdz",
+    "good": ["Ail", "Artichaut", "Asperge", "Carotte", "Chou", "Courge", "Potiron", "Courgette", "Mais", "Navet", "Oignon", "Poireau", "Radis"],
+    "bad": []
+  },
+  {
+    "name": "Mache",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/laitue.svg",
+    "desc": "blabla bla blabla blalblalbla  zqdqzd zqdqz dqzdq zqd",
+    "good": ["Poireau", "Tomate"],
+    "bad": []
+  },
+  {
+    "name": "Mais",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/mais.svg",
+    "desc": "blabla bla blabla blalblalbla loiuytre dezfrgtyu ",
+    "good": ["Concombre", "Courge", "Potiron", "Courgette", "Laitue", "Tomate"],
+    "bad": ["Betterave"]
+  },
+  {
+    "name": "Navet",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/navet.svg",
+    "desc": "blabla bla blabla blalblalbla oiuyt ezdsqdqsdza",
+    "good": ["Asperge", "Betterave", "Carotte", "Courge", "Potiron", "Courgette", "Laitue", "Pois"],
+    "bad": []
+  },
+  {
+    "name": "Oignon",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/oignon.svg",
+    "desc": "blabla bla blabla blalblalbla zefdzf fsd fefdsqzdzwdf",
+    "good": ["Artichaut", "Asperge", "Betterave", "Carotte", "Concombre", "Epinard", "Fraisier", "Laitue", "Poireau", "Tomate"],
+    "bad": ["Haricot", "Pois"]
+  },
+  {
+    "name": "Persil",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/persil.svg",
+    "desc": "blabla bla blabla blalblalbla dfsd sdfds gftrfeazzedsasqd",
+    "good": ["Asperge", "Tomate"],
+    "bad": ["Pois"]
+  },
+  {
+    "name": "Poireau",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/poireau.svg",
+    "desc": "blabla bla blabla blalblalbla dsq dqs dqsdzaefdrgfd ",
+    "good": ["Carotte", "Céleri", "Epinard", "Laitue", "Mache", "Oignon", "Tomate"],
+    "bad": ["Haricot"]
+  },
+  {
+    "name": "Pois",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/pois.svg",
+    "desc": "blabla bla blabla blalblalbla dza sqd fsgdfhgfdf dsqdqsd",
+    "good": ["Artichaut", "Asperge", "Betterave", "Carotte", "Chou", "Concombre", "Epinard", "Navet", "Pomme de terre", "Radis"],
+    "bad": ["Ail", "Ciboulette", "Oignon", "Persil"]
+  },
+  {
+    "name": "Pomme de terre",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/patate.svg",
+    "desc": "blabla bla blabla blalblalbla sqd sqgtrytrsdsqf ",
+    "good": ["Chou", "Feve", "Haricot", "Pois", "Radis", "Raifort"],
+    "bad": ["Concombre", "Courge", "Potiron", "Courgette", "Tomate"]
+  },
+  {
+    "name": "Radis",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/radis.svg",
+    "desc": "blabla bla blabla blalblalbla dzqdsqfgt yhfgtdsscd",
+    "good": ["Artichaut", "Asperge", "Carotte", "Céleri", "Cerfeuil", "Concombre", "Epinard", "Laitue", "Pois", "Pomme de terre", "Tomate"],
+    "bad": ["Chou", "Haricot"]
+  },
+  {
+    "name": "Raifort",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/raifort.svg",
+    "desc": "blabla bla blabla blalblalbla dz qdsqd qgfhfds sdgferdgdsf",
+    "good": ["Pomme de terre"],
+    "bad": []
+  },
+  {
+    "name": "Tomate",
+    "ground": ["argile", "sable", "normal"],
+    "season" : "",
+    "img": "/assets/svg/tomate.svg",
+    "desc": "La tomate (Solanum lycopersicum L.) est une espèce de plantes herbacées de la famille des Solanacées, originaire du Nord-Ouest de l'Amérique du Sud, largement cultivée pour son fruit.",
+    "good": ["Ail", "Asperge", "Carotte", "Céleri", "Chou", "Mache", "Mais", "Oignon", "Persil", "Poireau", "Radis"],
+    "bad": ["Concombre", "Fenouil", "Haricot", "Pomme de terre"]
+  }
+];
